fc2ce4c52e51a8a75c47f46594a0e452a37ef433
Iyin0 <p2iyinoluwa@gmail.com>
2023-03-10 12:24:14 +0100

    classwork

--- Class-2/projectTwo/objects.js
+++ Class-2/projectTwo/objects.js
@@ -22,3 +22,15 @@ console.log(user.children)
 console.log(user.first_name)
 
 console.log(user.logChildren())
+
+
+// TASK
+// 1. Write a function that takes two numbers as inputs and returns the sum of all the even numbers between them. Use a loop and conditional statements to accomplish this task.
+
+// 2. Write a program that prints the first 100 Fibonacci numbers. Use a loop and conditional statements to accomplish this task.
+
+// 3. Write a program that takes an array of integers and returns the largest number in the array. Use a loop and conditional statements to accomplish this task.
+
+// 4. Write a program that takes an array of integers and returns the sum of all the even numbers in the array. Use a loop and conditional statements to accomplish this task.
+
+// 5. Write a program that takes an array of integers and returns a new array with only the even numbers. Use a loop and conditional statements to accomplish this task.
