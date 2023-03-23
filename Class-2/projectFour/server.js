8f8b3f944e51e4751172b119516922222c0ceaf8
Iyin0 <p2iyinoluwa@gmail.com>
2023-03-23 11:41:00 +0100

    request and response

--- Class-2/projectFour/server.js
+++ Class-2/projectFour/server.js
@@ -1,20 +1,45 @@
 // const os = require('os')
 
-// // console.log(os.version)
+// console.log(os)
 // console.log('The current directory is ' + __dirname)
 // console.log('The current file is ' + __filename)
 
-// // console.log('Welcome to NodeJS Class')
+// console.log('Welcome to NodeJS Class')
 
-// // console.log(2 + 5)
+// console.log(2 + 5)
 
 const http = require('http')
 
 // to create the server
 const server = http.createServer((req, res) => {
-    console.log('Request Made')
-    res.write('Request Made to the server on the backend')
-    res.end()
+    res.setHeader('Content-Type', 'text/plain')
+
+    switch (req.url) {
+        case '/':
+            res.write('Homepage')
+            res.statusCode(200)
+            res.end()
+            break;
+        case '/login':
+            res.write('Login Page')
+            res.statusCode(200)
+            res.end()
+            break;
+        case '/create':
+            res.write('Create Account page')
+            res.statusCode(200)
+            res.end()
+            break;
+        case '/about':
+            res.write('About us page')
+            res.statusCode(200)
+            res.end()
+            break;
+        default:
+            res.write('404 page')
+            res.statusCode(404)
+            res.end()
+    }
 })
 
 // listen for request
