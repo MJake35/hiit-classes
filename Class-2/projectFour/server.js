a772f6a45d380312b22e17499a133c8f3f758b41
Iyin0 <p2iyinoluwa@gmail.com>
2023-03-29 10:46:45 +0100

    http module

--- Class-2/projectFour/server.js
+++ Class-2/projectFour/server.js
@@ -9,35 +9,61 @@
 // console.log(2 + 5)
 
 const http = require('http')
+const fs = require('fs')
 
-// to create the server
+// to create the server         https://dev.mysql.com/downloads/windows/
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain')
+    // res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Type', 'text/html')
 
     switch (req.url) {
         case '/':
-            res.write('Homepage')
-            res.statusCode(200)
-            res.end()
+            fs.readFile('./views/index.html', (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+                res.write(data);
+                res.end();
+            })
             break;
         case '/login':
-            res.write('Login Page')
-            res.statusCode(200)
+            // res.write('Login Page')
+            res.write('<h1>Login</h1>')
+            res.write('<p>Login to our website</p>')
+            // res.statusCode(200)
             res.end()
             break;
         case '/create':
-            res.write('Create Account page')
-            res.statusCode(200)
+            // res.write('Create Account page')
+            res.write('<h1>Sign up</h1>')
+            res.write('<p>Sign up to our website</p>')
+            // res.statusCode(200)
             res.end()
             break;
         case '/about':
-            res.write('About us page')
-            res.statusCode(200)
-            res.end()
+            fs.readFile('./views/aboutUS.html', (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+                res.write(data);
+                res.end();
+            })
+            break;
+        case '/contact':
+            fs.readFile('./views/contact.html', (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+                res.write(data);
+                res.end();
+            })
+            break;
+        case '/terms':
+            fs.readFile('./views/termAndCondition.html', (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+                res.write(data);
+                res.end();
+            })
             break;
         default:
-            res.write('404 page')
-            res.statusCode(404)
+            // res.write('404 page')
+            res.write('<h1>404</h1>')
+            res.write('<p>Oops! The content is not found</p>')
+            // res.statusCode(404)
             res.end()
     }
 })
