ede9fa672006fc59d13fd2e9b5763dd205473938
Iyin0 <p2iyinoluwa@gmail.com>
2023-03-30 12:44:23 +0100

    introduction to API

--- Class-2/express/app.js
+++ Class-2/express/app.js
@@ -1,17 +1,19 @@
 const express = require('express') // import express package
 const morgan = require('morgan')
 require('dotenv').config()      // importing the dot env file
+const bodyParser = require('body-parser')
 
 const app = express()       // creating an instance of express (class)
 const PORT = process.env.PORT       // using the dot env file
 
 // Middlewares
 app.use(morgan('dev'))
-app.use((req, res, next) => {
-    console.log('Request made')
+// app.use((req, res, next) => {
+//     console.log('Request made')
 
-    next()
-})
+//     next()
+// })
+app.use(bodyParser.json())      // used to identify the body of a request
 
 
 app.get('/', (req, res) => {
@@ -22,8 +24,19 @@ app.get('/about', (req, res) => {
     res.status(200).send('This is the About')
 })
 
-app.get('/contact', (req, res) => {
-    res.status(200).send('This is the Contact')
+app.post('/contact', (req, res, next) => {
+    const { first_name, last_name, email, phone, body } = req.body
+
+    try {
+        if (!first_name) throw Error('Input your first name')
+        if (!last_name) throw Error('Input your last name')
+        if (!email) throw Error('Input your email address')
+        if (!phone) throw Error('Input your phone number')
+        if (!body) throw Error('Input the message body')
+        res.status(200).json({ message: 'Your enquiry has been recorded. We will reach out to you shortly.' })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 app.get('/terms', (req, res) => {
@@ -31,9 +44,9 @@ app.get('/terms', (req, res) => {
 })
 
 
-app.use((req, res) => {
-    res.status(404).send('This is the 404')
-})
+// app.use((req, res) => {
+//     res.status(404).send('This is the 404')
+// })
 
 
 app.listen(PORT, () => {
